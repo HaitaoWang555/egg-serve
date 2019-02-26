0160289692a903a4ef85e21fd941ff96fa8712bc
wanghaitao <15124505701@163.com>
2019-02-26 17:32:04 +0800

    JSDOC

--- app/error/base_http.js
+++ app/error/base_http.js
@@ -1,6 +1,13 @@
 'use strict';
 
 class BaseHttp extends Error {
+  /**
+   * HTTP 请求错误处理
+   * @param {*} msg 信息
+   * @param {*} code 自定义错误码
+   * @param {*} httpMsg 提示信息
+   * @param {*} httpStatusCode HTTP 状态码
+   */
   constructor(msg, code, httpMsg, httpStatusCode) {
     super(msg);
     this.code = code || BaseHttp.CODE;
