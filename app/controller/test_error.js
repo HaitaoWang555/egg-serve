5b5cbd016cba22ff8dfec348e22f928481b9a111
wanghaitao <15124505701@163.com>
2019-02-26 17:32:18 +0800

    use APIdoc

--- app/controller/test_error.js
+++ app/controller/test_error.js
@@ -1,18 +1,36 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const InvalidParam = require('../error/invalid_param');
+const Response = require('../utils/variable').response;
 
 class ErrorController extends Controller {
-  /**
-   * @param {STRING} param 测试错误处理函数，无参数会有错误提示
-   */
+/**
+ * @apiVersion 0.1.0
+ * @api {get} /test_error 测试错误处理函数
+ * @apiParam {String} param 参数
+ *
+ * @apiGroup test
+ *
+ * @apiError {Integer} code 响应码
+ * @apiError {Object} param 参数
+ * @apiErrorExample Error-Response:
+ * {"code":40003,"msg":"没有参数param"}
+ *
+ * @apiSuccess {Integer} code 响应码
+ * @apiSuccess {Object} param 参数
+ * @apiSuccessExample Success-Response:
+ * {"code":0,"msg":"成功","data":{"param": "111111"}}
+ */
   async index() {
-    const { ctx, app } = this;
+    const { ctx } = this;
     const { param } = ctx.query;
     if (!param) {
-      throw new app.error.InvalidParam('param', 'param is STRING', '没有参数param');
+      throw new InvalidParam('param', 'param is STRING', '没有参数param');
     } else {
-      ctx.body = `<h3>hello ${param}</h3>`;
+      const res = Response;
+      res.data.param = param;
+      ctx.body = res;
     }
   }
 }
