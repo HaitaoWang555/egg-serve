ae64426c2da5a88008c25238b5f450b399e14468
wanghaitao <15124505701@163.com>
2019-02-26 15:01:05 +0800

    revision

--- app/controller/test_error.js
+++ app/controller/test_error.js
@@ -7,10 +7,10 @@ class ErrorController extends Controller {
    * @param {STRING} param 测试错误处理函数，无参数会有错误提示
    */
   async index() {
-    const { ctx } = this;
-    const { param } = this.ctx.query;
+    const { ctx, app } = this;
+    const { param } = ctx.query;
     if (!param) {
-      throw new this.app.error.InvalidParam('param', 'param is STRING', '没有参数param');
+      throw new app.error.InvalidParam('param', 'param is STRING', '没有参数param');
     } else {
       ctx.body = `<h3>hello ${param}</h3>`;
     }
