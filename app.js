f016fe92ef915cf1311d08ff7e03ab6e96e824a9
wanghaitao <15124505701@163.com>
2019-02-26 17:31:37 +0800

    not use app.loader

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-'use strict';
-const path = require('path');
+// 'use strict';
+// const path = require('path');
 
-module.exports = app => {
-  // const mpdelPaths = app.loader.getLoadUnits().map(unit => path.join(unit.path, 'app/model'));
-  // app.loader.loadToContext(mpdelPaths, 'model');
-  app.loader.loadToApp(path.join(app.config.baseDir, 'app/error'), 'error', {
-    caseStyle: 'upper',
-  });
-};
+// module.exports = app => {
+//   // const mpdelPaths = app.loader.getLoadUnits().map(unit => path.join(unit.path, 'app/model'));
+//   // app.loader.loadToContext(mpdelPaths, 'model');
+//   app.loader.loadToApp(path.join(app.config.baseDir, 'app/error'), 'error', {
+//     caseStyle: 'upper',
+//   });
+// };
